03b1b6e26f06ef219800a117c4822335e4f65fa8
Daniel Arzamendia <darzamendia6@gmail.com>
2023-06-26 01:22:32 -0300

    Purga de bootstrap y generación de componente input - Afterclass

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,76 @@
-import "./App.css";
-import Header from "./components/header";
-import ItemListContainer from "./components/itemlistcontainer/itemlistcontainer";
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header'
+import ItemListContainer from './components/itemlistcontainer/itemlistcontainer'
+import Counter from './components/counter/counter'
+import Input from './components/input/input'
 
 function App() {
-    return (
-        <>
-            <Header logo="Keeb me!" />
-            <ItemListContainer 
-                itemTitleOne   = "SP-Star Meteor White"     itemPriceOne   = "231" itemStockOne   = "100"
-                itemTitleTwo   = "Novelkeys Cream Linear"   itemPriceTwo   = "247" itemStockTwo   = "50"
-                itemTitleThree = "C³Equalz X TKC Tangerine" itemPriceThree = "260" itemStockThree = "25"
-            />
-        </>
-    );
+	const [counter, setCounter] = useState(0)
+	const [task, setTask] = useState('')
+	const [active, setActive] = useState(false)
+
+	const isValidCounter = counter > 0
+
+	const incrementCounter = () => {
+		setCounter((prevCounter) => prevCounter + 1)
+	}
+
+	const decrementCounter = () => {
+		if (!isValidCounter) return
+		setCounter((prevCounter) => prevCounter - 1)
+	}
+
+	const setTast = () => {}
+
+	const onChange = (event) => {
+		const value = event.target.value
+		setTast(value)
+	}
+
+	const onFocus = () => {
+		setActive(true)
+	}
+
+	const onBlur = () => {
+		setActive(false)
+	}
+
+	const inputClass = `container ${active ? 'active' : ''}`
+
+	return (
+		<>
+			<Header logo="Keeb me!" />
+			<ItemListContainer
+				itemTitleOne="SP-Star Meteor White"
+				itemPriceOne="231"
+				itemStockOne="100"
+				itemTitleTwo="Novelkeys Cream Linear"
+				itemPriceTwo="247"
+				itemStockTwo="50"
+				itemTitleThree="C³Equalz X TKC Tangerine"
+				itemPriceThree="260"
+				itemStockThree="25"
+			/>
+			<Counter
+				counter={counter}
+				onDecrementCounter={decrementCounter}
+				onIncrementCounter={incrementCounter}
+				isValidCounter={isValidCounter}
+			/>
+			<div style={{ width: '50%', padding: '10px' }}>
+				<Input
+					cntClassName={inputClass}
+					placeholder="Ingrese su nombre completo..."
+					labelName="Nombre completo"
+					id="fullName"
+					onChange={onChange}
+					onFocus={onFocus}
+					onBlur={onBlur}
+				/>
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/input/input.jsx
@@ -0,0 +1,32 @@
+import './input.css'
+
+const Input = ({
+	id,
+	placeholder,
+	type = 'text',
+	required = false,
+	labelName,
+	onFocus,
+	onChange,
+	onBlur,
+	value,
+	cntClassName,
+}) => {
+	return (
+		<div className={cntClassName}>
+			<input
+				id={id}
+				type={type}
+				placeholder={placeholder}
+				required={required}
+				onFocus={onFocus}
+				onChange={onChange}
+				onBlur={onBlur}
+				value={value}
+			/>
+			<label htmlFor={id}>{labelName}</label>
+		</div>
+	)
+}
+
+export default Input
