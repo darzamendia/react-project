fc105e5024dacc0954dc616992dd4e3995b59151
Daniel Arzamendia <darzamendia6@gmail.com>
2023-08-05 18:03:04 -0300

    meh2

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+// import firebase from 'firebase/compat/app';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -11,27 +12,19 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // const firebaseConfig = {
 // 	apiKey: import.meta.env.VITE_APP_API_KEY,
-// 	authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+// 	autDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
 // 	projectId: import.meta.env.VITE_APP_PROJECT_ID,
 // 	storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
 // 	messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
 // 	appId: import.meta.env.VITE_APP_APP_ID,
 // };
+
 const firebaseConfig = {
-	// apiKey: 'AIzaSyCFrrl7Q1zHa4nra0TQye1nqTaY0cQney0',
-	apiKey: import.meta.apiKey,
-	autDomain: import.meta.autDomain,
-	// authDomain: 'project-coderhouse-40fe9.firebaseapp.com',
-	// projectId: import.meta.projectId,
+	apiKey: 'AIzaSyCFrrl7Q1zHa4nra0TQye1nqTaY0cQney0',
+	authDomain: 'project-coderhouse-40fe9.firebaseapp.com',
 	projectId: 'project-coderhouse-40fe9',
-
 	storageBucket: 'project-coderhouse-40fe9.appspot.com',
-	// storageBucket: import.meta.storageBucket,
-
 	messagingSenderId: '761716474951',
-	// messagingSenderId: import.meta.messagingSenderId,
-
-	// appId: import.meta.appId,
 	appId: '1:761716474951:web:32f3bc02e1bf83271f1d62',
 };
 // Initialize Firebase
