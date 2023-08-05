215d639f422012d57acc14f928cc1afbda85633f
Daniel Arzamendia <darzamendia6@gmail.com>
2023-08-05 02:00:59 -0300

    Try to fix firebase with github pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/header/header';
 import { CartProvider } from './context/cart-context';
 import Navigation from './navigation/navigation';
+
 function App() {
 	return (
 		<div>
