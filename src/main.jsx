d012c299baf583cdca792470dfeb0671f53e3dfa
Daniel Arzamendia <darzamendia6@gmail.com>
2023-08-05 19:04:22 -0300

    bye

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-// import firebase from 'firebase/compat/app';
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
